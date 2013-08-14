ccb47cfcdce230d1992677fb613a6b609a9e09b4
Linda Nguyen <skoryky@gmail.com>
2013-08-14 16:29:53 -0700

    Initialize Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PatientVitals::Application.config.secret_key_base = '2b11800182b4d7b1518c307638c06a86aa1c00d3283c4e35d0852b08e0ca142f6b4fc88597ef76f507101921c0a366487b3fbbde780bffc1547716fab4f2f88e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PatientVitals::Application.config.session_store :cookie_store, key: '_patient-vitals_session'
